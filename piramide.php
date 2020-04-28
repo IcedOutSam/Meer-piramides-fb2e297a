75dd26beca4b826d8df356a4a76b382503896a19
Sam <2102926@talnet.nl>
2020-04-28 18:25:16 +0200

    Uitwerking van de exercise

---
+++ piramide.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Deep Cover</title>
+  </head>
+  <body>
+<table>
+  <?php
+  for ($tr=0; $tr <= 9; $tr++) {
+echo "<tr>";
+
+    for ($W=0; $W < $tr; $W++) {
+      echo "<td class=blokwit height=50px width=50px bgcolor=#FF0000></td>";
+    }
+  echo "</tr>";
+  }
+   ?>
+
+</table>
+  </body>
+</html>
